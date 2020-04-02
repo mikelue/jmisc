f64ecd4b2839dc53d5fe9a4b59f02c6943c7160a
Mike Lue <mike.lue0627@msa.hinet.net>
2020-04-02 14:36:05 +0800

    Add retriever for data source to test base of JPA repository

--- testlib/src/main/java/guru/mikelue/misc/testlib/jpa/AbstractJpaReposTestBase.java
+++ testlib/src/main/java/guru/mikelue/misc/testlib/jpa/AbstractJpaReposTestBase.java
@@ -2,28 +2,41 @@ package guru.mikelue.misc.testlib.jpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
 import guru.mikelue.misc.testlib.AbstractTestBase;
 
+/**
+ * Depending on Springframework, this base class provides
+ * {@link #getEntityManager()} and {@link #getDataSource()} to provide
+ * convenient method for testing.
+ */
 @Transactional
 public abstract class AbstractJpaReposTestBase extends AbstractTestBase {
 	@PersistenceContext
 	private EntityManager em;
 
+	@Autowired
+	private DataSource ds;
+
 	protected AbstractJpaReposTestBase() {}
 
+	/**
+	 * Retrieves the {@link EntityManager} under current application context.
+	 */
 	public EntityManager getEntityManager()
 	{
 		return em;
 	}
 
-	public EntityManager getEm() {
-		return em;
-	}
-
-	public void setEm(final EntityManager em) {
-		this.em = em;
+	/**
+	 * Retrieves the {@link DataSource} under current application context.
+	 */
+	public DataSource getDataSource()
+	{
+		return ds;
 	}
 }
